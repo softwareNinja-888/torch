123f59f620227ab9d2b6d83c8d02aa7082bf9eb9
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-03-07 19:54:32 +0200

    Initial commit

---
+++ torch/src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-primary w-screen h-screen">
+        <div className="text-6xl ">Hello World!!!</div>
+      </div>
+    </>
+  )
+}
+
+export default App
